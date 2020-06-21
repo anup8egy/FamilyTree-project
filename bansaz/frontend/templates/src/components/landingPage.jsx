95a6935ede5b13cd687819d5a4ffd93392796d24
anup8egy <anup8eguy@gmail.com>
2020-06-21 06:57:41 +0545

    no changes

--- bansaz/frontend/templates/src/components/landingPage.jsx
+++ bansaz/frontend/templates/src/components/landingPage.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 export default class LandingPage extends Component {
   render() {
-    return <div>Landed</div>;
+    return (
+      <section className="content">
+        <div className="header">Kul</div>
+        <div className="subHeader">
+          Kul, the clan App. Find or create your clan.
+        </div>
+        
+      </section>
+    );
   }
 }
