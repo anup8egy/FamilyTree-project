4f1acb6754c6f3e23017d793aba062a1c3e8fb1a
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2020-06-21 08:11:43 +0545

    Made fontuseable

--- bansaz/frontend/templates/src/App.js
+++ bansaz/frontend/templates/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/landingPage";
 import Error from "./components/error";
 import Navbar from "./components/navBar";
-import "./fonts/fonts.css";
+// import "./fonts/fonts.css";
 export default class App extends Component {
   render() {
     return (
