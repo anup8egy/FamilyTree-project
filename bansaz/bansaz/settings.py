a7a6edcd395182a974c64e20e70175197e638af3
anup8egy <anup8eguy@gmail.com>
2020-06-26 20:14:42 +0545

    just pushed to pull

--- bansaz/bansaz/settings.py
+++ bansaz/bansaz/settings.py
@@ -49,7 +49,6 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
-        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
 }
 MIDDLEWARE = [
